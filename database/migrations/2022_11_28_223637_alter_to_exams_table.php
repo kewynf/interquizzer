64f43fdafb7c87ab38c259fb00d7a4fbc4138824
Kewyn Ferreira <47131740+kewynf@users.noreply.github.com>
2022-11-28 19:39:24 -0300

    Create 2022_11_28_223637_alter_to_exams_table.php

---
+++ database/migrations/2022_11_28_223637_alter_to_exams_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('exams', function (Blueprint $table) {
+            $table->timestamp('started_at')->nullable()->after('discord_text_channel_id');
+            $table->timestamp('ended_at')->nullable()->after('started_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('exams', function (Blueprint $table) {
+            $table->dropColumn('started_at');
+            $table->dropColumn('ended_at');
+        });
+    }
+};
