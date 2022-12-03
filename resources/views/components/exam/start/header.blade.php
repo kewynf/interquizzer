75f3cbac904d885971aefc3678b9a097d2389cfc
Kewyn Ferreira <47131740+kewynf@users.noreply.github.com>
2022-12-03 08:37:22 -0300

    Add Exam Start Header Component

---
+++ resources/views/components/exam/start/header.blade.php
@@ -0,0 +1,4 @@
+<div class="flex flex-col w-full p-6 bg-white dark:bg-gray-800">
+    <h1 class="text-gray-600 dark:text-gray-100 text-2xl font-bold">{{ $exam->title }}</h1>
+    <h2 class="text-gray-500 dark:text-gray-300 text-md">{{ $exam->description }}</h2>
+</div>
